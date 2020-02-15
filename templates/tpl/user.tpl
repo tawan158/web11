8d1ae0c11ebb41ec597414d7ac11d6ea5369ac35
tawan158 <tawan158@ugm.com.tw>
2020-02-15 14:15:51 +0800

    20200215_5

---
+++ templates/tpl/user.tpl
@@ -0,0 +1,31 @@
+<{if $op=="op_list"}>
+    <table class="table table-striped table-bordered table-hover table-sm">
+        <thead>
+        <tr>
+            <th scope="col">帳號</th>
+            <th scope="col">姓名</th>
+            <th scope="col">電話</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">狀態</th>
+            <th scope="col">功能</th>
+        </tr>
+        </thead>
+        <tbody>
+            <{foreach $rows as $row}>
+                <tr>
+                    <td><{$row.uname}></td>
+                    <td><{$row.name}></td>
+                    <td><{$row.tel}></td>
+                    <td><{$row.email}></td>
+                    <td><{$row.kind}></td>
+                    <td></td>
+                </tr>
+            <{foreachelse}>
+                <tr>
+                    <td colspan=6>目前沒有資料</td>
+                </tr>
+            <{/foreach}>
+
+        </tbody>
+    </table>
+<{/if}>
--- templates_c/5896a3e43df6cee7f5111e531ae63244189cdcd1_0.file.user.tpl.php
+++ templates_c/5896a3e43df6cee7f5111e531ae63244189cdcd1_0.file.user.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-02-15 06:51:49
+/* Smarty version 3.1.34-dev-7, created on 2020-02-15 07:13:45
   from 'D:\ugm\xampp\htdocs\web11\templates\tpl\user.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5e4786f54795d6_69650932',
+  'unifunc' => 'content_5e478c19922eb0_39940562',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5896a3e43df6cee7f5111e531ae63244189cdcd1' => 
     array (
       0 => 'D:\\ugm\\xampp\\htdocs\\web11\\templates\\tpl\\user.tpl',
-      1 => 1581745903,
+      1 => 1581747220,
       2 => 'file',
     ),
   ),
@@ -20,6 +20,51 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5e4786f54795d6_69650932 (Smarty_Internal_Template $_smarty_tpl) {
-?>adsfsdfadsfasdf<?php }
+function content_5e478c19922eb0_39940562 (Smarty_Internal_Template $_smarty_tpl) {
+if ($_smarty_tpl->tpl_vars['op']->value == "op_list") {?>
+<table class="table table-striped table-bordered table-hover table-sm">
+    <thead>
+      <tr>
+        <th scope="col">帳號</th>
+        <th scope="col">姓名</th>
+        <th scope="col">電話</th>
+        <th scope="col">EMAIL</th>
+        <th scope="col">狀態</th>
+        <th scope="col">功能</th>
+      </tr>
+    </thead>
+    <tbody>
+        <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['rows']->value, 'row');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['row']->value) {
+?>
+            <tr>
+                <td><?php echo $_smarty_tpl->tpl_vars['row']->value['uname'];?>
+</td>
+                <td><?php echo $_smarty_tpl->tpl_vars['row']->value['name'];?>
+</td>
+                <td><?php echo $_smarty_tpl->tpl_vars['row']->value['tel'];?>
+</td>
+                <td><?php echo $_smarty_tpl->tpl_vars['row']->value['email'];?>
+</td>
+                <td><?php echo $_smarty_tpl->tpl_vars['row']->value['kind'];?>
+</td>
+                <td></td>
+            </tr>
+        <?php
+}
+} else {
+?>
+            <tr>
+                <td colspan=6>目前沒有資料</td>
+            </tr>
+        <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+
+    </tbody>
+  </table>
+<?php }
+}
 }
