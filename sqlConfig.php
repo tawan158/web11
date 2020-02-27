119f4596b72e417035c0bbe49e4e4228523e9a11
tawan158 <tawan158@ugm.com.tw>
2020-02-27 16:29:47 +0800

    20200227_12

--- sqlConfig.php
+++ sqlConfig.php
@@ -1,12 +1,25 @@
 <?php
-#資料庫伺服器
-$db_host = "localhost";
-#資料庫使用者帳號
-$db_user = "root";
-#資料庫使用者密碼
-$db_password = "111111";
-#資料庫名稱
-$db_name = "web11";
+if($_SERVER['HTTP_HOST'] == "127.0.0.1" or "localhost"){
+  #資料庫伺服器
+  $db_host = "localhost";
+  #資料庫使用者帳號
+  $db_user = "root";
+  #資料庫使用者密碼
+  $db_password = "111111";
+  #資料庫名稱
+  $db_name = "web11";
+}else{
+  #資料庫伺服器
+  $db_host = "localhost";
+  #資料庫使用者帳號
+  $db_user = "root";
+  #資料庫使用者密碼
+  $db_password = "111111";
+  #資料庫名稱
+  $db_name = "web11";
+}
+
+
 #PHP 5.2.9以後
 $db = new mysqli($db_host, $db_user, $db_password, $db_name); 
 if ($db->connect_error) {
