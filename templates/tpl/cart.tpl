8e88061e3712d245e5b14c73f4b8b6033050e5dd
tawan158 <tawan158@ugm.com.tw>
2020-03-04 13:15:11 +0800

    20200304_4

--- templates/tpl/cart.tpl
+++ templates/tpl/cart.tpl
@@ -18,7 +18,7 @@
                 <{$row.title}>： <{$row.price}> 元
               </div>
               <div class="mt-2">
-                <a href="#" class="btn btn-primary btn-sm">加入購物車</a>
+                <a href="#" class="btn btn-primary btn-sm" onclick="add_cart(<{$row.sn}>);">加入購物車</a>
               </div>
             </div>
           </div>
@@ -31,5 +31,27 @@
 
   </div>
   <!-- /.container -->
+  
+  <!-- sweetalert2 -->
+  <link rel="stylesheet" href="<{$xoAppUrl}>class/sweetalert2/sweetalert2.min.css">
+  <script src="<{$xoAppUrl}>class/sweetalert2/sweetalert2.min.js"></script>
+  <script>
+    function add_cart(sn){
+      Swal.fire({
+        title: '加入購物車？',
+        // text: "您將無法還原！",
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '是的',
+        cancelButtonText: '取消'
+        }).then((result) => {
+        if (result.value) {
+            document.location.href="cart.php?op=add_cart&sn="+sn;
+        }
+      })
+    }
+</script>
 <{elseif  $op == "Portfolio"}>
 <{/if}>
--- templates_c/50e4048a5f846acadf4dda68c273764db420ec2f_0.file.cart.tpl.php
+++ templates_c/50e4048a5f846acadf4dda68c273764db420ec2f_0.file.cart.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-03-04 11:36:58
+/* Smarty version 3.1.34-dev-7, created on 2020-03-04 13:14:26
   from 'D:\ugm\xampp\htdocs\web11\templates\tpl\cart.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5e5f225a3e3e07_50248014',
+  'unifunc' => 'content_5e5f3932549d51_40587191',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '50e4048a5f846acadf4dda68c273764db420ec2f' => 
     array (
       0 => 'D:\\ugm\\xampp\\htdocs\\web11\\templates\\tpl\\cart.tpl',
-      1 => 1583293002,
+      1 => 1583298859,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5e5f225a3e3e07_50248014 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5e5f3932549d51_40587191 (Smarty_Internal_Template $_smarty_tpl) {
 if ($_smarty_tpl->tpl_vars['op']->value == "op_list") {?>
   <!-- Page Content -->
   <div class="container" style="margin-top: 110px;">
@@ -48,7 +48,8 @@ foreach ($_from as $_smarty_tpl->tpl_vars['row']->value) {
  元
               </div>
               <div class="mt-2">
-                <a href="#" class="btn btn-primary btn-sm">加入購物車</a>
+                <a href="#" class="btn btn-primary btn-sm" onclick="add_cart(<?php echo $_smarty_tpl->tpl_vars['row']->value['sn'];?>
+);">加入購物車</a>
               </div>
             </div>
           </div>
@@ -65,6 +66,34 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
 
   </div>
   <!-- /.container -->
+  
+  <!-- sweetalert2 -->
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['xoAppUrl']->value;?>
+class/sweetalert2/sweetalert2.min.css">
+  <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['xoAppUrl']->value;?>
+class/sweetalert2/sweetalert2.min.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+>
+    function add_cart(sn){
+      Swal.fire({
+        title: '加入購物車？',
+        // text: "您將無法還原！",
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '是的',
+        cancelButtonText: '取消'
+        }).then((result) => {
+        if (result.value) {
+            document.location.href="cart.php?op=add_cart&sn="+sn;
+        }
+      })
+    }
+<?php echo '</script'; ?>
+>
 <?php } elseif ($_smarty_tpl->tpl_vars['op']->value == "Portfolio") {
 }
 }
